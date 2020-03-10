911f9633e473d6cc63babb017c749d9fa115f64f
darrylgit <darrylsmithup@gmail.com>
2020-03-10 13:56:57 -0400

    Toggle cart dropdown

--- src/index.js
+++ src/index.js
@@ -6,12 +6,13 @@ import { PersistGate } from "redux-persist/integration/react";
 import { ApolloProvider } from "react-apollo";
 import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
-import { ApolloClient, gql } from "apollo-boost";
+import { ApolloClient } from "apollo-boost";
 
 import { store, persistor } from "./redux/store";
 
 import "./index.css";
 import App from "./App";
+import { resolvers, typeDefs } from "./graphql/resolvers";
 
 const httpLink = createHttpLink({
   uri: "https://crwn-clothing.com"
@@ -21,25 +22,16 @@ const cache = new InMemoryCache();
 
 const client = new ApolloClient({
   link: httpLink,
-  cache
+  cache,
+  typeDefs,
+  resolvers
 });
 
-client
-  .query({
-    query: gql`
-      {
-        getCollectionsByTitle(title: "jackets") {
-          id
-          title
-          items {
-            name
-            price
-          }
-        }
-      }
-    `
-  })
-  .then(res => console.log(res));
+client.writeData({
+  data: {
+    cartHidden: true
+  }
+});
 
 ReactDOM.render(
   <ApolloProvider client={client}>
